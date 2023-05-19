15759f5361b5511da94ac5ff8f112ab2f18d513f
brett-buskirk <buskirkbrett8@gmail.com>
2023-05-18 20:06:36 -0700

    Initial project commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
+import MUI from './MUI';
+import Blog from './blog/Blog';
 import reportWebVitals from './reportWebVitals';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import '@fontsource/roboto/700.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Blog />
   </React.StrictMode>
 );
 
